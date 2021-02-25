7fe7b8d66d3b950176d2ad4bb7dca1141871003b
eugeneshark <yevhenii.skakun@gmail.com>
2021-02-25 14:58:13 +0100

    Added apearence and useability of code

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from tkinter import *
+from win32api import GetSystemMetrics
+
+screen_width = GetSystemMetrics(0)
+screen_height = GetSystemMetrics(1)
+
+window_width = 900
+window_height = 600
+window_min_width = window_width
+window_min_height = window_height
+
+margin_left = int(screen_width / 2 - window_min_width / 2)
+margin_top = int(screen_height / 2 - window_min_height / 2)
+
+main_background_color = "#212121"
+
+label_info_background_color = "#212121"
+label_info_font_color = "white"
 
 window = Tk()
 
 window.title("Python Arduino LED")
+window.geometry("{}x{}+{}+{}".format(window_width, window_height, margin_left, margin_top))
 
-window.geometry("900x600")
+window.minsize(width=window_min_width, height=window_min_height)
+window.configure(bg=main_background_color)
 
-window.configure(bg = "orange")
 
-Label(text = "This is a Label", bg = "black", fg = "white").pack()
 
-Button(text = "Click me", bg = "white", fg = "black").pack()
+info_label = Label(text = "This is a Label", bg = label_info_background_color, fg=label_info_font_color, width=window_width, font=("Verdana", 14)).pack(side=BOTTOM)
+
 
 
 window.mainloop()
