ba2fe7934249a847645c5e5cdc53a57d1a0a22fe
eugeneshark <yevhenii.skakun@gmail.com>
2021-03-02 01:11:04 +0100

    Added functions to display color and effect buttons

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import tkinter as tk
 from tkinter import ttk 
 from win32api import GetSystemMetrics
 
+
+WORKING_SERIAL_PORT = None
+
 def serial_ports():
     """ Lists serial port names
 
@@ -38,15 +41,20 @@ def serial_ports():
 
 def choose_com_port():
     choosen_port = com_port_combo_box.get()
+    global WORKING_SERIAL_PORT
+    WORKING_SERIAL_PORT = choosen_port
     print(choosen_port)
 
 def set_color():
     pass
 
+def set_effect():
+    pass
+
 def set_random_color():
     pass
 
-WORKING_SERIAL_PORT = None
+
 
 screen_width = GetSystemMetrics(0)
 screen_height = GetSystemMetrics(1)
@@ -64,15 +72,24 @@ main_background_color = "#212121"
 label_info_background_color = "#212121"
 label_info_font_color = "white"
 
-color_list = ["white", "blue", "red", "yellow", "purple", "light blue", "green", "aqua", "violet", "orange", "pink", "fuchsia", "lime", "cyan"]
-effects_list = ["fire", "fade", "running lights", ""]
+colors_list = ["white", "blue", "red", "yellow", "purple", "light blue", "green", "aqua", "violet", "orange", "pink", "fuchsia", "lime", "cyan", "off"]
+effects_list = ["fire immitation", "fading lights", "running lights", "rain effect", "equalizer mode"]
+
+color_button_x_margin = 400
+color_button_y_margin = 70
+
+effect_button_x_margin = 30
+effect_button_y_margin = 300
+
+button_font = ("Verdana", 10)
 
 window = Tk()
-window.title("Python Arduino LED")
+window.title("Python Arduino LED v1.0.0")
 window.geometry("{}x{}+{}+{}".format(window_width, window_height, margin_left, margin_top))
 window.minsize(width=window_min_width, height=window_min_height)
 window.configure(bg=main_background_color)
-
+# add resizable in future
+window.resizable(0, 0)
 
 n = tk.StringVar() 
 com_port_combo_box = ttk.Combobox(window, width = 27,  
@@ -81,7 +98,7 @@ com_port_combo_box = ttk.Combobox(window, width = 27,
 com_port_combo_box['values'] = ('Please choose COM port',  
                           ) 
 
-choose_com_port_button = Button(text ="Choose COM port", command = choose_com_port)
+choose_com_port_button = Button(text ="Choose COM port", command = choose_com_port, font=button_font)
 
 # get list of all COM ports
 connected_serial_ports = serial_ports()
@@ -98,8 +115,8 @@ red_slider.config(highlightbackground="#212121")
 green_slider.config(highlightbackground="#212121")
 blue_slider.config(highlightbackground="#212121")
 
-set_color_button = Button(text ="Set color", command = set_color())
-set_random_color_button = Button(text ="Random color", command = set_random_color())
+set_color_button = Button(text ="Set color", command = set_color(), font=button_font)
+set_random_color_button = Button(text ="Random color", command = set_random_color(), font=button_font)
 
 # Adding combobox drop down list 
 com_port_combo_box.place(x=10, y=10)
@@ -107,10 +124,35 @@ choose_com_port_button.place(x=200,y=10)
 red_slider.place(x=10, y=50)
 green_slider.place(x=10, y=100)
 blue_slider.place(x=10, y=150)
+
 set_random_color_button.place(x=15, y=200)
 set_color_button.place(x=255, y=200)
 
 com_port_combo_box.current()  
+
+
+for color in colors_list:
+    color_button = Button(text =color, command = set_color(), font=button_font) 
+    if color_button_x_margin >= 800:
+        color_button.place(x=color_button_x_margin, y=color_button_y_margin)
+        color_button_y_margin += 50
+        color_button_x_margin = 400
+    else:
+        color_button.place(x=color_button_x_margin, y=color_button_y_margin)
+        color_button_x_margin += 100
+
+effects_label = Label(text = "Effects", bg = label_info_background_color, fg=label_info_font_color, font=("Verdana", 14))
+effects_label.place(x = int(window_width/2 - 50), y = 250)
+
+for effect in effects_list:
+    effect_button = Button(text =effect, command = set_effect(), font=button_font) 
+    if effect_button_x_margin >= 800:
+        effect_button.place(x=effect_button_x_margin, y=effect_button_y_margin)
+        effect_button_y_margin += 50
+        effect_button_x_margin = 30
+    else:
+        effect_button.place(x=effect_button_x_margin, y=effect_button_y_margin)
+        effect_button_x_margin += 120
 
 info_label = Label(text = "Info messages", bg = label_info_background_color, fg=label_info_font_color, font=("Verdana", 14))
 info_label.place(x = int(window_width/2), y = int(window_height - 75))
